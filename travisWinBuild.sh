6b3616a18a6eea47c92bf819f4c0cf6a6f2d0a02
Patrick Boyd <Patrick_Boyd@Dell.com>
2019-02-06 16:33:23 -0600

    Fix typo in script

--- travisWinBuild.sh
+++ travisWinBuild.sh
@@ -1,7 +1,7 @@
 #Get CURL
 wget https://curl.haxx.se/windows/dl-7.64.0/curl-7.64.0-win64-mingw.zip
-7z x curl-7.63.0-win64-mingw.zip
-mv curl-7.63.0-win64-mingw curl
+7z x curl-7.64.0-win64-mingw.zip
+mv curl-7.64.0-win64-mingw curl
 #Do Build
 mkdir build
 cd build 
