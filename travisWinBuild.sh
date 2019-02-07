75ec18072885b50ad31b5fcb5c5ceb33f8abaa6d
Patrick Boyd <Patrick_Boyd@Dell.com>
2019-02-07 11:09:09 -0600

    Move curl to windows lib name

--- travisWinBuild.sh
+++ travisWinBuild.sh
@@ -3,6 +3,7 @@ echo "Downloading CURL..."
 wget https://curl.haxx.se/windows/dl-7.64.0/curl-7.64.0-win64-mingw.zip
 7z x curl-7.64.0-win64-mingw.zip
 mv curl-7.64.0-win64-mingw curl
+cp curl/lib/libcurl.a curl/lib/curl.lib
 echo "Completed downloading CURL"
 #Get Jansson
 echo "Downloading Jansson..."
@@ -14,6 +15,6 @@ echo "Completed downloading Jansson..."
 mkdir build
 cd build 
 echo "Configuring..."
-cmake .. -DCURL_LIBRARY=../curl/lib/libcurl.dll.a -DCURL_INCLUDE_DIR=../curl/include -DJANSSON_LIBRARIES=../jansson.lib -DJANSSON_INCLUDE_DIRS=../
+cmake .. -DCURL_LIBRARY=../curl/lib/curl.lib -DCURL_INCLUDE_DIR=../curl/include -DJANSSON_LIBRARIES=../jansson.lib -DJANSSON_INCLUDE_DIRS=../
 echo "Building..."
 cmake --build . --config Release
