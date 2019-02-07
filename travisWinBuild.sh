6cbdbf261059300e2be228edede7161ebb0a9426
Patrick Boyd <Patrick_Boyd@Dell.com>
2019-02-07 12:27:39 -0600

    Add getopt and hopefull fix x64 build

--- travisWinBuild.sh
+++ travisWinBuild.sh
@@ -1,10 +1,13 @@
+#!/bin/sh -x
 #Get CURL
 echo "Downloading CURL..."
 if [[ "$VISUAL_STUDIO" == "Visual Studio 15 2017 Win64" ]]; then
+PLATFORM=x64
 wget https://curl.haxx.se/windows/dl-7.64.0/curl-7.64.0-win64-mingw.zip
 7z x curl-7.64.0-win64-mingw.zip
 mv curl-7.64.0-win64-mingw curl
 else
+PLATFORM=x86
 wget https://curl.haxx.se/windows/dl-7.64.0/curl-7.64.0-win32-mingw.zip
 7z x curl-7.64.0-win32-mingw.zip
 mv curl-7.64.0-win32-mingw curl
@@ -17,11 +20,15 @@ wget -O jansson.lib https://github.com/pboyd04/jansson/releases/download/v2.11/$
 wget -O include/jansson.h https://github.com/pboyd04/jansson/releases/download/v2.11/jansson.h
 wget -O include/jansson_config.h https://github.com/pboyd04/jansson/releases/download/v2.11/jansson_config.h
 echo "Completed downloading Jansson..."
+#Get getopt.h for windows
+echo "Downloading getopt.h for windows..."
+wget -O include/getopt.h https://raw.githubusercontent.com/skandhurkat/Getopt-for-Visual-Studio/master/getopt.h
+echo "Completed downloading getopt.h for windows..."
 #Do Build
 mkdir build
 cd build 
 echo "Configuring..."
-cmake .. -DCURL_LIBRARY=../curl/lib/curl.lib -DCURL_INCLUDE_DIR=../curl/include -DJANSSON_LIBRARIES=../jansson.lib -DJANSSON_INCLUDE_DIRS=../
+cmake .. -DCMAKE_GENERATOR_PLATFORM=$PLATFORM -DCURL_LIBRARY=../curl/lib/curl.lib -DCURL_INCLUDE_DIR=../curl/include -DJANSSON_LIBRARIES=../jansson.lib -DJANSSON_INCLUDE_DIRS=../
 echo "Building..."
 cmake --build . --config Release
 echo "Making deployment directory..."
